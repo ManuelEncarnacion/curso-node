768cc9bbf39ede122926bcfabf6d9c316b763097
Manuel <sistemas.manuelencarnacion@gmail.com>
2021-02-13 03:41:20 -0600

    primer commit

---
+++ app.js
@@ -0,0 +1,16 @@
+
+
+
+const { crearArchivo } = require('./helpers/multiplicar');
+const argv = require('./config/yargs');
+
+
+console.clear();
+
+
+
+crearArchivo( argv.base, argv.l, argv.h)
+    .then( nombreArchivo => console.log( nombreArchivo, 'creado ') )
+    .catch( err => console.log( err ) );
+
+   
---
+++ helpers/multiplicar.js
@@ -0,0 +1,36 @@
+
+const fs = require('fs');
+const { resolve } = require('path');
+const colors = require('colors');
+
+const crearArchivo = async (base = 5, listar, hasta) => {  
+    try{
+        let salida, consola = '';
+        if(listar){
+            console.log("===============".green);
+            console.log('Tabla del ', colors.blue( base ) );
+            console.log("===============".underline.green);
+
+            for( let indice = 0 ; indice<=hasta; indice++ ){
+
+                salida+= `Valor de ${base} X ${indice} = ${ base * indice } \n` ;
+                consola+= `Valor de ${base} ${ 'X'.random } ${indice} = ${ base * indice } \n` ;
+   
+            }
+
+            console.log(consola.italic.green);
+        }
+        
+        fs.writeFileSync( `./salida/tabla-${base  }.txt`, salida);
+
+        return `tabla-${base  }.txt`;
+    }catch ( error ) {
+        throw error;
+    }
+    
+
+}
+
+module.exports = {
+    crearArchivo
+}
